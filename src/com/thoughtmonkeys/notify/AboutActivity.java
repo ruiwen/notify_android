f35a37f98c489d8d7b23a910223e918479801d93
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-04-15 02:27:14 +0800

    Tracking views of the About page

--- src/com/thoughtmonkeys/notify/AboutActivity.java
+++ src/com/thoughtmonkeys/notify/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.thoughtmonkeys.notify;
 
 
+import com.google.analytics.tracking.android.EasyTracker;
 import android.os.Bundle;
 import android.app.Activity;
 
@@ -10,6 +11,21 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
+	}
+
+	@Override
+	protected void onStart() {
+	
+		super.onStart();		
+		EasyTracker.getInstance().activityStart(this);
+	}
+
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		
+		EasyTracker.getInstance().activityStop(this);
 	}
 
 }
