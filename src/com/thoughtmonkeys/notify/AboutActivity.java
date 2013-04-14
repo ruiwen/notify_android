9c64b4c984c4cc4cb9be4b466c096dc79de1bc27
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-04-15 02:31:07 +0800

    Adding feedback button in About page
    Prompts user to choose an email client and prepopulates a message to apps@thoughtmonkeys.com with a subject and message body

--- src/com/thoughtmonkeys/notify/AboutActivity.java
+++ src/com/thoughtmonkeys/notify/AboutActivity.java
@@ -2,8 +2,12 @@ package com.thoughtmonkeys.notify;
 
 
 import com.google.analytics.tracking.android.EasyTracker;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
 
 public class AboutActivity extends Activity {
 
@@ -27,5 +31,20 @@ public class AboutActivity extends Activity {
 		
 		EasyTracker.getInstance().activityStop(this);
 	}
+
+
+	public void sendFeedback(View view) {
+
+		// Track the send feedback button click
+		EasyTracker.getTracker().sendEvent("app_action", "send_feedback", "", 1L);
+	
+		// Kick off a new Intent to send feedback
+		Intent feedbackIntent = new Intent(android.content.Intent.ACTION_SENDTO, Uri.fromParts("mailto", getString(R.string.feedback_email), null));
+		feedbackIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.feedback_subject));
+		feedbackIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.feedback_body));
+		startActivity(Intent.createChooser(feedbackIntent, getString(R.string.feedback_chooser_title)));
+
+	}
+	
 
 }
