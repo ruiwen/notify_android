7a6e36162bcfc381d8bcadc143e1c58ae156ec1e
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-07-16 02:38:51 +0800

    fix(parsers) fixing up BaseParser
     - New view ID
     - Ensuring results HashMap is instantiated
     - Ensuring we unpack and attach (reapply) the remoteViews

--- src/com/thoughtmonkeys/notify/parsers/BaseParser.java
+++ src/com/thoughtmonkeys/notify/parsers/BaseParser.java
@@ -13,6 +13,7 @@ public class BaseParser {
 
 	protected int TITLE_ID = 16908310;
 	protected int TEXT_ID = 16908358;
+	protected int BIGTEXT_ID = 16909097;
 	protected int INBOX0_ID = 16909101;
 	protected int INBOX1_ID = 16909102;
 	protected int INBOX2_ID = 16909103;
@@ -21,7 +22,7 @@ public class BaseParser {
 	protected int INBOX5_ID = 16909106;
 	protected int INBOX6_ID = 16909107;
 	protected int INBOXMORE_ID = 16909108;
-
+	
 	protected RemoteViews remoteViews;
 	protected LayoutInflater inflater;
 	protected ViewGroup localViews;
@@ -40,6 +41,7 @@ public class BaseParser {
 
 	public HashMap<String, String> parse() {
 		
+		this.results = new HashMap<String, String>();
 		this.results.put("packageName", this.packageName);
 		
 		String[] tickerText = this.notification.tickerText.toString().split(": ");
@@ -61,6 +63,7 @@ public class BaseParser {
 		
 			this.inflater = (LayoutInflater) this.context.getSystemService(this.context.LAYOUT_INFLATER_SERVICE);
 	        this.localViews = (ViewGroup) inflater.inflate(remoteViews.getLayoutId(), null);
+	        this.remoteViews.reapply(this.context, localViews);
 		}
 	    catch(NullPointerException e) {
 	    }
