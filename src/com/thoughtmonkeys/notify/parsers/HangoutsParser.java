78fd871c9bf211998cb3a2abe544f7d7d5b12c2a
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-07-16 02:41:27 +0800

    fix(parsers) refining Hangouts parser

--- src/com/thoughtmonkeys/notify/parsers/HangoutsParser.java
+++ src/com/thoughtmonkeys/notify/parsers/HangoutsParser.java
@@ -2,8 +2,13 @@ package com.thoughtmonkeys.notify.parsers;
 
 import java.util.HashMap;
 
+import com.thoughtmonkeys.notify.R;
+
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.TextView;
 
 public class HangoutsParser extends BaseParser {
 
@@ -14,18 +19,34 @@ public class HangoutsParser extends BaseParser {
 
 	@Override
 	public HashMap<String, String> parse() {
-		this.results = super.parse();
-		this.componentise();
+		super.parse();
+		super.componentise();
 		
 		try {
+			String notification_title = ((TextView) this.localViews.findViewById(TITLE_ID)).getText().toString();
 			
+			if(this.results.get("text").contains(", ")) {
+				return this.results;
+			}
 			// Multiple on-going chats
-			if(this.notification.tickerText.toString().matches("new messages")) {
-				
+			// INBOX0_ID should be populated
+			else if(notification_title.contains("new messages")) {
+				String txt = ((TextView) this.localViews.findViewById(INBOX0_ID)).getText().toString();	
+				this.results.put("text", txt);
+			}
+		
+			// Try for BIGTEXT_ID
+			else {
+				Log.d(this.context.getString(R.string.log_tag), "BIGTEXT");
+				String txt = ((TextView) this.localViews.findViewById(BIGTEXT_ID)).getText().toString();	
+				String[] parts = txt.split("[\\r\\n]+");
+				this.results.put("text", parts[parts.length-1]);
 			}
 		
 		}
 		catch(NullPointerException e) {
+			Log.d(this.context.getString(R.string.log_tag), e.toString());
+			Log.d(this.context.getString(R.string.log_tag), "" + e.getMessage());
 		}
 		
 		return this.results;
