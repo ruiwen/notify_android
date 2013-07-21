cb507a7dace95bc6a505da54ec79975f288240d7
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-07-22 03:26:45 +0800

    fix(parsers) improve parsing for WhatsApp messages

--- src/com/thoughtmonkeys/notify/parsers/WhatsAppParser.java
+++ src/com/thoughtmonkeys/notify/parsers/WhatsAppParser.java
@@ -6,6 +6,7 @@ import com.thoughtmonkeys.notify.R;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.TextView;
 
@@ -18,7 +19,9 @@ public class WhatsAppParser extends BaseParser {
 
 	@Override
 	public HashMap<String, String> parse() {
-	
+
+		Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()]");
+		
 		super.parse();
 		super.componentise();
 
@@ -27,33 +30,59 @@ public class WhatsAppParser extends BaseParser {
 			String notification_title = ((TextView) this.localViews.findViewById(TITLE_ID)).getText().toString();
 			// txt is expected to be of the format:
 			// <contact name> @ <chat group name>: <message>
-			String txt = ((TextView) this.localViews.findViewById(INBOX0_ID)).getText().toString();
-			
-			// Multiple on-going chats
-			//if(notification_title.equals("WhatsApp")) {
-			if(txt.contains("@")) {
+			String txt = "";
+			try {
+				txt = ((TextView) this.localViews.findViewById(BIGTEXT_ID)).getText().toString();
+				Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()] Use BIGTEXT");
 				
-				String[] parts = txt.split(": ");
-				String msg = parts[1];
-				parts = parts[0].split(" @ ");
-				msg = parts[0] + ": " + msg;
-				String title = parts[1];
+				// This is the first new WhatsApp notification
+				this.results.put("title", notification_title);
+				this.results.put("text", txt);				
+			}
+			catch (NullPointerException e) {
+				txt = ((TextView) this.localViews.findViewById(INBOX0_ID)).getText().toString();
+				Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()] Use INBOX0");
+
+				// Multiple on-going chats
+				if(notification_title.contains("WhatsApp")) { 
+					if(this.notification.tickerText.toString().contains("@")) {
+						Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()] ticker has @");
+						
+						String[] parts = txt.split(": ");
+						String msg = parts[1];
+						parts = parts[0].split(" @ ");
+						msg = parts[0] + ": " + msg;
+						String title = parts[1];
+						
+						this.results.put("title", title);
+						this.results.put("text", msg);					
+					}
+	
+					else if(txt.contains(": ")) {
+						String[] parts = txt.split(": ", 2);
+						
+						if(parts.length == 2) {
+							Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()]] 2 parts");
+							this.results.put("title", parts[0]);
+							this.results.put("text",  parts[1]);
+						}
+					}
+				}
 				
-				this.results.put("title", title);
-				this.results.put("text", msg);
-				
+				else {
+					Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()] fallthrough");
+					
+					this.results.put("title", notification_title);
+					this.results.put("text", txt);
+				}
 			}
 
-			// only one on-going chat group
-			else {
-				String[] parts = txt.split(": ");
-				this.results.put("title", parts[0]);
-				this.results.put("text", parts[1]);
-				
-			}
+			Log.d(this.context.getString(R.string.log_tag), "title: "+ notification_title);
+			Log.d(this.context.getString(R.string.log_tag), "text: "+ txt);
 			
 		}
 		catch(NullPointerException e) {
+			Log.d(this.context.getString(R.string.log_tag), "[debug WhatsAppParser:parse()] parse() failed");
 			Log.d(this.context.getString(R.string.log_tag), e.toString());
 		}
 
